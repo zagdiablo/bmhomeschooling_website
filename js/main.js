a10dfcea5cee9b45803dcf0911ff72c2cfb5a7e1
Reyshal <reyke.svb@gmail.com>
2022-03-13 15:28:59 +0700

    javascript commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector('header nav .hamburger');
 const mobileMenu = document.querySelector('header nav ul');
-const menuItem = document.querySelector('header nav ul li a');
+const menuItems = document.querySelectorAll('header nav ul li a');
 const header = document.querySelector('header');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileMenu.classList.toggle('active');
-})
+});
 
 document.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
@@ -16,4 +16,11 @@ document.addEventListener('scroll', () => {
     } else {
         header.style.backgroundColor = 'transparent';
     }
+});
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    })
 })
